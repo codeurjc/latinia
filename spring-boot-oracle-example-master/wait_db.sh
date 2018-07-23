de011ee19677905f54d45e4d8854a46b64b5f6c3
nordri <nordri@gmail.com>
2018-07-23 16:32:14 +0200

    Change database ready script

--- spring-boot-oracle-example-master/wait_db.sh
+++ spring-boot-oracle-example-master/wait_db.sh
@@ -4,7 +4,7 @@ sleep 1m
 
 while true
 do 
-  docker logs oracledb | grep "DATABASE IS READY TO USE!"
+  docker logs oracledb | grep "Completed: ALTER DATABASE OPEN"
   if [ $? == 0 ]; then
     break
   fi
