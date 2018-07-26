8155ba55df204cd6c29724bcf895d0545002a7f7
nordri <nordri@gmail.com>
2018-07-26 19:01:37 +0200

    Title string

--- websphere/gauge/src/test/java/ComprobarComponentes.java
+++ websphere/gauge/src/test/java/ComprobarComponentes.java
@@ -22,7 +22,7 @@ public class ComprobarComponentes {
     @Step("comprobar titulo")
     public void Comprobar() {
 
-        String texto = "Sample \"Hello, World\" Application";
+        String texto = "WebSphere Liberty 18.0.0.2";
 
         // Se realiza la consulta si el titulo es LATINIA si lo es informa que existe el titulo,
         // en caso de que no, sea informa de un ERROR
