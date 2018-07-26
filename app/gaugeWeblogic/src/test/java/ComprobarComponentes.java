ff4eee50097963e8d6b4acc48f5cbb5b3d13d092
nordri <nordri@gmail.com>
2018-07-26 17:42:32 +0200

    Title

--- app/gaugeWeblogic/src/test/java/ComprobarComponentes.java
+++ app/gaugeWeblogic/src/test/java/ComprobarComponentes.java
@@ -22,7 +22,7 @@ public class ComprobarComponentes {
     @Step("comprobar titulo")
     public void Comprobar() {
 
-        String texto = 'Sample "Hello, World" Application';
+        String texto = "Sample \"Hello, World\" Application";
 
         // Se realiza la consulta si el titulo es LATINIA si lo es informa que existe el titulo,
         // en caso de que no, sea informa de un ERROR
