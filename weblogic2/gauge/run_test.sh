1c1bc9f0de247cc537c8b818b7837c0532277012
nordri <nordri@gmail.com>
2018-09-04 11:30:14 +0200

    Adjusting path in script

--- weblogic2/gauge/run_test.sh
+++ weblogic2/gauge/run_test.sh
@@ -2,15 +2,15 @@
 
 /usr/bin/Xvfb :99 -screen 0 1024x768x16 &
 
-cd ${WORKSPACE}/app/gaugeWeblogic
+cd ${WORKSPACE}/weblogic2/gauge
 gauge run specs/0.1Comprobar.spec > output.txt
 
 cat output.txt
 
-RES=$(cat ${WORKSPACE}/app/gaugeWeblogic/output.txt | grep "ERR:")
+RES=$(cat ${WORKSPACE}/weblogic2/gauge/output.txt | grep "ERR:")
 
 # Saving reports
-tar czf ${WORKSPACE}/reports.tar.gz ${WORKSPACE}/app/gaugeWeblogic/reports/html-report
+tar czf ${WORKSPACE}/reports.tar.gz ${WORKSPACE}/weblogic2/gauge/reports/html-report
 
 if [ z"$RES" == z"" ]; then
   exit 0
