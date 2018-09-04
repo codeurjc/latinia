81768fd4861157408c0b807e29f261b2a92e3798
nordri <nordri@gmail.com>
2018-09-04 10:54:33 +0200

    Adjusting path in script

--- websphere2/gauge/run_test.sh
+++ websphere2/gauge/run_test.sh
@@ -2,15 +2,15 @@
 
 /usr/bin/Xvfb :99 -screen 0 1024x768x16 &
 
-cd ${WORKSPACE}/websphere/gauge
+cd ${WORKSPACE}/websphere2/gauge
 gauge run specs/0.1Comprobar.spec > output.txt
 
 cat output.txt
 
-RES=$(cat ${WORKSPACE}/websphere/gauge/output.txt | grep "ERR:")
+RES=$(cat ${WORKSPACE}/websphere2/gauge/output.txt | grep "ERR:")
 
 # Saving reports
-tar czf ${WORKSPACE}/reports.tar.gz ${WORKSPACE}/websphere/gauge/reports/html-report
+tar czf ${WORKSPACE}/reports.tar.gz ${WORKSPACE}/websphere2/gauge/reports/html-report
 
 if [ z"$RES" == z"" ]; then
   exit 0
