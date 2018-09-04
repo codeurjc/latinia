7e44b0b95f4267bb68b899aabb634a8ca267f816
nordri <nordri@gmail.com>
2018-09-04 09:13:42 +0200

    From JAR to WAR

--- spring-boot-oracle-example-master/src/main/java/guru/springframework/SpringBootOracleApplication.java
+++ spring-boot-oracle-example-master/src/main/java/guru/springframework/SpringBootOracleApplication.java
@@ -2,9 +2,15 @@ package guru.springframework;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
-public class SpringBootOracleApplication {
+public class SpringBootOracleApplication extends SpringBootServletInitializer {
+
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringBootOracleApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootOracleApplication.class, args);
